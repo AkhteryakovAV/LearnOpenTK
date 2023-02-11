37426fcd8bb403ab89aa276f07775e0101c8a9b6
AkhteryakovAV <akhteryakov@mail.ru>
2023-02-11 17:47:37 +0400

    Add project files.

---
+++ Common/Shader.cs
@@ -0,0 +1,124 @@
+﻿using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Shader
+    {
+        //Дескриптор будет представлять местоположение нашей окончательной шейдерной программы
+        //после завершения ее компиляции.
+        private int _handle;
+        private bool _disposedValue = false;
+        public Shader(string vertexPath, string fragmentPath)
+        {
+            CreateShaderItem(vertexPath, ShaderType.VertexShader, out int vertexShader);
+            CreateShaderItem(fragmentPath, ShaderType.FragmentShader, out int fragmentShader);
+
+            CompileShader(vertexShader);
+            CompileShader(fragmentShader);
+
+            AttachShaders(new int[] { vertexShader, fragmentShader });
+
+            //Отдельные вершинные и фрагментные шейдеры теперь бесполезны, поскольку они связаны;
+            //скомпилированные данные копируются в шейдерную программу при их связывании. 
+            GL.DetachShader(_handle, vertexShader);
+            GL.DetachShader(_handle, fragmentShader);
+            GL.DeleteShader(vertexShader);
+            GL.DeleteShader(fragmentShader);
+        }
+
+        /// <summary>
+        /// Связывает отдельные шейдеры в одну программу, которую можно запускать на графическом процессоре.
+        /// </summary>
+        /// <param name="shaders"></param>
+        /// <exception cref="Exception"></exception>
+        private void AttachShaders(int[] shaders)
+        {
+            _handle = GL.CreateProgram();
+            foreach (var shader in shaders)
+            {
+                GL.AttachShader(_handle, shader);
+            }
+            GL.LinkProgram(_handle);
+            GL.GetProgram(_handle, GetProgramParameterName.LinkStatus, out int success);
+            if (success != (int)All.True)
+            {
+                string infoLog = GL.GetProgramInfoLog(_handle);
+                throw new Exception(infoLog);
+            }
+        }
+
+        /// <summary>
+        /// Создание шейдера
+        /// </summary>
+        /// <param name="path">Путь к файлу шейдера</param>
+        /// <param name="shader">Указатель на скомпилированный шейдер</param>
+        /// <exception cref="Exception"></exception>
+        /// <exception cref="FileNotFoundException"></exception>
+        private static void CreateShaderItem(string path, ShaderType shaderType, out int shader)
+        {
+            if (File.Exists(path))
+            {
+                string shaderSource = File.ReadAllText(path);
+                if (string.IsNullOrEmpty(shaderSource))
+                {
+                    throw new Exception("Файл шейдера пустой");
+                }
+                //генерируем шейдеры 
+                shader = GL.CreateShader(shaderType);
+                //Привязываем исходный код к шейдерам.
+                GL.ShaderSource(shader, shaderSource);
+            }
+            else
+            {
+                throw new FileNotFoundException(path);
+            }
+        }
+
+        /// <summary>
+        /// Компилируем шейдеры и проверяем их на наличие ошибок.
+        /// </summary>
+        /// <param name="shader">Указатель на скомпилированный шейдер</param>
+        /// <exception cref="Exception">При ошибки компиляции</exception>
+        private static void CompileShader(int shader)
+        {
+            GL.CompileShader(shader);
+            GL.GetShader(shader, ShaderParameter.CompileStatus, out int success);
+            if (success != (int)All.True)
+            {
+                string infoLog = GL.GetShaderInfoLog(shader);
+                throw new Exception($"Не удалось скомпилировать шейдер: {nameof(shader)}.{Environment.NewLine}Лог: {infoLog}");
+            }
+        }
+
+        public void Use()
+        {
+            GL.UseProgram(_handle);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                GL.DeleteProgram(_handle);
+                _disposedValue = true;
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~Shader()
+        {
+            GL.DeleteProgram(_handle);
+        }
+    }
+}
---
+++ LearnOpenTK/Game.cs
@@ -0,0 +1,81 @@
+﻿using Common;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnOpenTK
+{
+    internal class Game : GameWindow
+    {
+        private int _vertexBufferObject;
+        private readonly float[] _vertices =
+        {
+            -0.5f, -0.5f, 0.0f,
+             0.5f, -0.5f, 0.0f,
+             0.0f,  0.5f, 0.0f
+        };
+        private Shader _shader;
+        public Game(int width, int height, string title)
+            : base(width, height, GraphicsMode.Default, title)
+        {
+        }
+
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+            KeyboardState keyboardState = Keyboard.GetState();
+            if (keyboardState.IsKeyDown(Key.Escape))
+            {
+                Exit();
+            }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+            //Создал бубер и сохранил дескриптор
+            _vertexBufferObject = GL.GenBuffer();
+
+            //Связал буффер.
+            //С этого момента любые вызовы буфера, которые мы делаем (для BufferTarget.ArrayBuffer цели),
+            //будут использоваться для настройки текущего связанного буфера, который является VertexBufferObject. 
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+            //Копирую данные в текущий связанный буфер
+            GL.BufferData<float>(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+
+            _shader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            //Code goes here.
+
+            Context.SwapBuffers();
+
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            GL.Viewport(0, 0, Width, Height);
+        }
+
+        protected override void OnUnload(EventArgs e)
+        {
+            base.OnUnload(e);
+            _shader.Dispose();
+        }
+    }
+}
