06ab13ef598a77de352cb76bd1008b5aed444164
AkhteryakovAV <akhteryakov@mail.ru>
2023-02-20 20:48:30 +0400

    Альтернативная последовательность действий

--- LearnOpenTK/Game.cs
+++ LearnOpenTK/Game.cs
@@ -46,34 +46,23 @@ namespace LearnOpenTK
             base.OnLoad(e);
             GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 
-            //Создал бубер и сохранил дескриптор
             _vertexBufferObject = GL.GenBuffer();
-
-            //Связал буффер.
-            //С этого момента любые вызовы буфера, которые мы делаем (для BufferTarget.ArrayBuffer цели),
-            //будут использоваться для настройки текущего связанного буфера, который является VertexBufferObject. 
-            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
-
-            //Копирую данные в текущий связанный буфер
-            GL.BufferData<float>(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
-
-            //Создал объек массива вершин и сохранил дескриптор
             _vertexArrayObject = GL.GenVertexArray();
-            //Привязка VAO 
-            // С этого момента мы должны привязать / настроить соответствующие VBO(ы) и указатели атрибутов,
-            // а затем отменить привязку VAO для последующего использования. Как только мы хотим нарисовать объект,
-            // мы просто привязываем VAO к предпочтительным настройкам перед рисованием объекта, и все. 
-            GL.BindVertexArray(_vertexArrayObject);
+            _elementBufferObject= GL.GenBuffer();
+            _shader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");
 
-            //Связывание атрибутов вершин
+            // 1. Привязываем VAO
+            GL.BindVertexArray(_vertexArrayObject);
+            // 2. Копируем наши вершины в буфер для OpenGL
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+            GL.BufferData<float>(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+            // 3. Копируем наши индексы в в буфер для OpenGL
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices, BufferUsageHint.StaticDraw);
+            // 3. Устанавливаем указатели на вершинные атрибуты
             GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
             GL.EnableVertexAttribArray(0);
 
-            _elementBufferObject= GL.GenBuffer();
-            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
-            GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices, BufferUsageHint.StaticDraw);
-
-            _shader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");
             _shader.Use();
         }
 
